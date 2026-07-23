26d4e53e41ad685928fecc3067a86faefffed652
ghassen-fatnassi <oneinbillion77@gmail.com>
2026-07-23 03:37:04 +0200

    wip

--- SGEMM_cuda/04_2d_block_tiling.cuh
+++ SGEMM_cuda/04_2d_block_tiling.cuh
@@ -22,57 +22,76 @@ __global__ void sgemm_04(int M,
                          const float *B,
                          float *C) {
 
-//keep a place where u are going to accumulate the elements
-// we'll keep a tile_result (which is basically per thread result, thread takescare of a 2D tile here)
-// so the tile_result=tilesize=TM*TN
-float reg_tile_result[TM][TN]={0.0};
-//and additionally we have to keep registers to put SMEM data we'll reuuse 
-float reg_A[TM]={0.0};
-float reg_B[TN]={0.0};
+    float reg_tile_result[TM][TN]={0.0};
+    float reg_A[TM]={0.0};
+    float reg_B[TN]={0.0};
 
-const uint cRow = blockIdx.y;
-const uint cCol = blockIdx.x;
+    const uint cRow = blockIdx.y;
+    const uint cCol = blockIdx.x;
 
-//coords of a thread that's responsible one 2D tile of the current block
-const uint threadCol = threadIdx.x / (BN/TN);
-const uint threadRow = threadIdx.x % (BN/TN);
+    //coords of a thread that's responsible one 2D tile of the current block
+    const uint threadCol = threadIdx.x / (BN/TN);
+    const uint threadRow = threadIdx.x % (BN/TN);
 
-const uint threads_per_block= (BM*BN)/(TM*TN);//easy to infer
-//look at it 1D it's easy
-//these pointers are pointing to where the work will start to populate the results of the current C block
-A += cRow * BM * K;                     
-B += cCol * BN;                        
-C += cRow * BM * N + cCol * BN;
+    const uint threads_per_block= (BM*BN)/(TM*TN);//easy to infer
+    //look at it 1D it's easy
+    //these pointers are pointing to where the work will start to populate the results of the current C block
+    A += cRow * BM * K;                     
+    B += cCol * BN;                        
+    C += cRow * BM * N + cCol * BN;
 
-//init SMEM of A and B
-__shared__ float shmemA[BM*BK];
-__shared__ float shmemB[BK*BN];//keep row-major
+    //init SMEM of A and B
+    __shared__ float shmemA[BM*BK];
+    __shared__ float shmemB[BK*BN];//keep row-major
 
-//you always always have to think from threadIdx.x to the data and to the computation seperately, difference between heaven and hell in mind-mapping
-//these indices are the whole game , pay attention if you are reading my repo
-const uint in_rowA = threadIdx.x / BK;
-const uint in_colA = threadIdx.x % BK;
-const uint offsetA_row_wise = threads_per_block/BK;
+    //you always always have to think from threadIdx.x to the data and to the computation seperately, difference between heaven and hell in mind-mapping
+    //these indices are the whole game , pay attention if you are reading my repo
+    const uint in_rowA = threadIdx.x / BK;
+    const uint in_colA = threadIdx.x % BK;
+    const uint offsetA_row_wise = threads_per_block/BK;
 
-const uint in_rowB= threadIdx.x / BN;
-const uint in_colB= threadIdx.x % BN;
-const uint offsetB_row_wise= threads_per_block/BN;
-//it's very important to watch ur indices because it's what decides coalescing , u need to make surethat lockstep loading from warps are contiguous
-//loop over BK
-for (blk_idx=0; blk_idx<=(K/BK); blk_idx++){
-    //even the shmem populating needs to happen in a loop
-    //the loop since eachthread is responsible for a 2D tile so each thread is also responsible for loaidng thatpart
-    for(int i=0;i<TM;++i){
-        shmemA[ threadIdx.x % BK ]=A[]
+    const uint in_rowB= threadIdx.x / BN;
+    const uint in_colB= threadIdx.x % BN;
+    const uint offsetB_row_wise= threads_per_block/BN;
+    //it's very important to watch ur indices because it's what decides coalescing , u need to make surethat lockstep loading from warps are contiguous
+    //loop over BK
+    for (blk_idx=0; blk_idx<=(K/BK); blk_idx++){
+        //even the shmem populating needs to happen in a loop
+        //the loop since eachthread is responsible for a 2D tile so each thread is also responsible for loaidng thatpart
+        for(uint i=0;i<BM;i+=offsetA_row_wise){
+            shmemA[(in_rowA+i)*BK+in_colA]=A[(in_rowA+i)*K+in_colA];// *K is very smart to wrap around
+        }
+        for(uint i=0;i<BK;i+=offsetB_row_wise){
+            shmemB[(in_rowA+)*BN+in_colB]=B[(in_rowB+i)*N+in_colB];// *N is also here for a wraparound
+        }
+        __syncthreads(); // the usual
+        A+=BK;
+        B+=BK*N;
+        //loop the number of time to do the outer product to accumulate
+        // think of it as the operation needing to preserve its lower bound complexity
+        // we are doing matmul between 2 small tiles: TM,BK and BK,TN , in our case they are all the same value
+        // let's call them just n,n * n,n meaning o(n^3)
+        // outer product being o(n²)(the 2 nexted inner loops) then we need another outer loop of accumulation to match the complexity
+        for(){
+            for(){
+                reg_A[]=shmemA[];
+            }
+            for(){
+                reg_B[]=shmemB[];
+            }
+            //2 loops since this is outer product
+            //this is the heart of reducing the number of loads from SMEM ( which caused the MIO throttle)
+            for(){
+                for(){
+                    reg_tile_result[]=reg_A[]*reg_B[];
+                }
+            }
+        }
+        __syncthreads();
     }
-    __syncthreads(); // the usual
-    A+=BK;
-    B+=BK*N;
-    //we didn't shit on coalescing since everything is 1D
-    //loop over TM
-        //loop over TN
-            //accumulate here
-    __syncthreads();
-}
-//return result to here
+    for(){
+        for(){
+            C[]=prod_scale*reg_tile_result[]+sum_scale*C[];
+        }
+    }
 }
